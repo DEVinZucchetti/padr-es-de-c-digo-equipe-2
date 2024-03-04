d2b1b6189a89c6e18d9dc31babb81b7723088812
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-03-04 13:02:32 -0300

    feat: cria as duas funções do controller de vaccine = listagem de todas as vacinas e criação de vacinas

--- app/Http/Repositories/VaccineRepository.php
+++ app/Http/Repositories/VaccineRepository.php
@@ -2,6 +2,17 @@
 
 namespace App\Http\Repositories;
 
+use App\Models\Vaccine;
+
 class VaccineRepository
 {
+    public function create(array $data)
+    {
+        return Vaccine::create($data);
+    }
+
+    public function getAll()
+    {
+        return Vaccine::all();
+    }
 }
