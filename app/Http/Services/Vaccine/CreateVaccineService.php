161dfe7e9056fb3080cb6e5dd6f868ff4c3b4595
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-03-04 22:01:48 -0300

    feat: finaliza sintaxe do service de vacinas

--- app/Http/Services/Vaccine/CreateVaccineService.php
+++ app/Http/Services/Vaccine/CreateVaccineService.php
@@ -1,4 +1,19 @@
 <?php
+
+use App\Http\Repositories\VaccineRepository;
+
 class CreateVaccineService
 {
+
+    private $vaccineRepository;
+
+    public function __construct(VaccineRepository $vaccineRepository)
+    {
+        $this->vaccineRepository = $vaccineRepository;
+    }
+
+    public function handle(array $data)
+    {
+        return $this->vaccineRepository->create($data);
+    }
 }
